7644aec18b4cb37f85231fd54655cb361f45d738
shaikhasif69 <theshaikhasif03@gmail.com>
2024-10-05 05:50:03 +0530

    nothing more

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,6 +20,7 @@ export const signup = async (req, res) => {
   } = req.body;
 
   try {
+    console.log("data here : " + username);
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
@@ -80,6 +81,7 @@ export const signin = async (req, res) => {
     res.status(201).json({ user: existingUser, token: token });
   } catch (error) {}
 };
+
 export const editProfile = async (req, res) => {
   const userId = req.userId;
   const {
